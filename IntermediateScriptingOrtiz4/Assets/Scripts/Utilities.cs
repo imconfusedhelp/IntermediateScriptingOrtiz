08a7fe96dd21e3063bc754fffeeb5a961e127855
imconfusedhelp <demskillzdough721129@gmail.com>
2024-01-24 11:10:51 -0800

    123123

---
+++ IntermediateScriptingOrtiz4/Assets/Scripts/Utilities.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static int Add(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
